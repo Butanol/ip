c053caf3469d4930a88b78ca22847260ce6f303d
Butanol <51089545+Butanol@users.noreply.github.com>
2025-08-22 02:54:01 +0800

    Add Done functionality
    Make tasks be able to be marked as done.

--- src/main/java/Shaniqua.java
+++ src/main/java/Shaniqua.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 
 public class Shaniqua {
     public static void main(String[] args) {
-        ArrayList<String> tasks = new ArrayList<String>();
+        String[] tasks = new String[100];
+        boolean[] done = new boolean[100];
+        int frontier = 0;
         Scanner input = new Scanner(System.in);
         System.out.print("Kia Ora! I'm Shaniqua!\nWhat can I do for you?\n");
         while (true) {
@@ -13,13 +15,29 @@ public class Shaniqua {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
             } else if (in.equals("list")) {
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.printf("%d. %s\n", i + 1, tasks.get(i));
+                for (int i = 0; i < frontier; i++) {
+                    if (tasks[i] != null) {
+                        System.out.printf("%d. [%s] %s\n", i + 1, done[i] ? "X" : " " , tasks[i]);
+                    }
                 }
+            } else if (in.contains("mark")) {
+                String[] temp = in.split(" ");
+                if (temp.length > 2) {
+                    System.out.println("Invalid mark input");
+                    continue;
+                }
+                int nums = Integer.parseInt(temp[1]);
+                done[nums] = !done[nums];
             } else {
-                tasks.add(in);
-                System.out.printf("Added: %s\n", in);
+                if (frontier < 100) {
+                    tasks[frontier] = in;
+                    frontier++;
+                    System.out.printf("Added: %s\n", in);
+                } else {
+                    System.out.println("Maximum capacity reached");
+                }
             }
         }
     }
 }
+
