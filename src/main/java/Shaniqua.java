b32979ae1792235efb895469373b9afebc00b586
Butanol <51089545+Butanol@users.noreply.github.com>
2025-08-22 02:38:00 +0800

    Add List functionality
    Make Shaniqua able to add any input that is not bye or list to the list; list signals for the program to list stored tasks.

--- src/main/java/Shaniqua.java
+++ src/main/java/Shaniqua.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 
+import java.util.ArrayList;
+
 public class Shaniqua {
     public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<String>();
         Scanner input = new Scanner(System.in);
         System.out.print("Kia Ora! I'm Shaniqua!\nWhat can I do for you?\n");
         while (true) {
@@ -9,8 +12,14 @@ public class Shaniqua {
             if (in.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
+            } else if (in.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.printf("%d. %s\n", i + 1, tasks.get(i));
+                }
+            } else {
+                tasks.add(in);
+                System.out.printf("Added: %s\n", in);
             }
-            System.out.println(in);
         }
     }
 }
