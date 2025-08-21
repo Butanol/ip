16df94aa2381e77bab8f9cd84932a26ba157f135
Butanol <51089545+Butanol@users.noreply.github.com>
2025-08-22 02:09:33 +0800

    Renamed template to Shaniqua.java
    Starting out the Individual Project.
    
    Remove all instances of Duke to Shaniqua.
    
    Shaniqua is a name that is pleasing to the ears.

---
+++ src/main/java/Shaniqua.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Shaniqua {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Hello! I'm Shaniqua!\nWhat can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
