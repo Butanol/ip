dd1bd148294ace6391e61f359159f00503cd5e9f
Butanol <51089545+Butanol@users.noreply.github.com>
2025-08-22 02:24:50 +0800

    Add Echo functionality
    Make Shaniqua able to echo any input that is not bye; bye signals for the program to close.

--- src/main/java/Shaniqua.java
+++ src/main/java/Shaniqua.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class Shaniqua {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Hello! I'm Shaniqua!\nWhat can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.print("Kia Ora! I'm Shaniqua!\nWhat can I do for you?\n");
+        while (true) {
+            String in = input.nextLine();
+            if (in.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            }
+            System.out.println(in);
+        }
     }
 }
