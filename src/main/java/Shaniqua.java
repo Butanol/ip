10f735a998df84fa830646c00f4311090fbae0ab
Butanol <51089545+Butanol@users.noreply.github.com>
2025-09-05 13:14:16 +0800

    Edit code for readability
    Edited code to be in line with Java Standards.

--- src/main/java/Shaniqua.java
+++ src/main/java/Shaniqua.java
@@ -10,46 +10,56 @@ public class Shaniqua {
                 String in = input.nextLine();
                 String[] commands = Shaniqua.handleInput(in);
                 switch (commands[0]) {
-                    case "bye" -> {
+                    case "bye":
                         System.out.println("Bye. Hope to see you again soon!");
-                        return;
-                    }
-                    case "list" -> tasks.list();
-                    case "mark" -> {
+                        break;
+                    case "list":
+                        tasks.list();
+                        break;
+                    case "mark":
                         if (!Shaniqua.integerCheck(commands[1])) {
                             throw new ChatException("Invalid task");
                         }
-                        int nums = Integer.parseInt(commands[1]);
-                        tasks.mark(nums);
-                        System.out.printf("Nice! I've marked this as done:\n%s\n", tasks.getTask(nums - 1));
-                    }
-                    case "unmark" -> {
+                        int idxA = Integer.parseInt(commands[1]);
+                        tasks.mark(idxA);
+                        System.out.printf("Nice! I've marked this as done:\n%s\n",
+                                tasks.getTask(idxA - 1));
+                        break;
+                    case "unmark":
                         if (!Shaniqua.integerCheck(commands[1])) {
                             throw new ChatException("Invalid task");
                         }
-                        int nums = Integer.parseInt(commands[1]);
-                        tasks.unmark(nums);
-                        System.out.printf("Nice! I've marked this as done:\n%s\n", tasks.getTask(nums - 1));
-                    }
-                    case "remove" -> {
+                        int idxB = Integer.parseInt(commands[1]);
+                        tasks.unmark(idxB);
+                        System.out.printf("Nice! I've marked this as done:\n%s\n",
+                                tasks.getTask(idxB - 1));
+                        break;
+                    case "remove":
                         if (!Shaniqua.integerCheck(commands[1])) {
                             throw new ChatException("Invalid task");
                         }
-                        int nums = Integer.parseInt(commands[1]);
-                        tasks.remove(nums);
-                    }
-                    case "todo" -> tasks.addTask(new Todo(commands[1]));
-                    case "deadline" -> {
-                        String[] params = Shaniqua.handleDeadline(commands[1]);
-                        tasks.addTask(new Deadline(params[0], params[1]));
-                    }
-                    case "event" -> {
-                        String[] params = Shaniqua.handleEvent(commands[1]);
-                        tasks.addTask(new Event(params[0], params[1], params[2]));
-                    }
+                        int idxC = Integer.parseInt(commands[1]);
+                        tasks.remove(idxC);
+                        break;
+                    case "todo":
+                        tasks.addTask(new Todo(commands[1]));
+                        break;
+                    case "deadline":
+                        String[] paramsDeadline = Shaniqua.
+                                handleDeadline(commands[1]);
+                        tasks.addTask(new Deadline(paramsDeadline[0],
+                                paramsDeadline[1]));
+                        break;
+                    case "event":
+                        String[] paramsEvents = Shaniqua.
+                                handleEvent(commands[1]);
+                        tasks.addTask(new Event(paramsEvents[0], paramsEvents[1],
+                                paramsEvents[2]));
+                        break;
                 }
-            } catch (UnsuccessfulException e){
-                System.out.println("Oh non, I can't do that ja. " + e.getMessage() + "!");
+            } catch (UnsuccessfulException e) {
+                System.out.println("Oh non, I can't do that ja. " + e.getMessage()
+                        + "!");
             } catch (ChatException e) {
                 System.out.println("Oops, I've got an error: " + e.getMessage());
             }
@@ -58,16 +68,16 @@ public class Shaniqua {
 
     /**
      *
-     * @param param an array of strings
+     * @param param input string
      * @return String without the first argument
      */
     private static String[] handleInput(String param) {
         String[] temp = param.split(" ");
-        StringBuilder x = new StringBuilder();
+        StringBuilder res = new StringBuilder();
         for (int i = 1; i < temp.length; i++) {
-            x.append(temp[i]);
+            res.append(temp[i]);
         }
-        return new String[]{temp[0], x.toString(), };
+        return new String[]{temp[0], res.toString()};
     }
 
     private static String[] handleDeadline(String param) throws ChatException {
@@ -75,7 +85,7 @@ public class Shaniqua {
         if (res.length == 1) throw new InsufficientException();
         return res;
     }
-    private static String[] handleEvent(String param) throws ChatException{
+    private static String[] handleEvent(String param) throws ChatException {
         String[] res = param.split("/by|/from");
         if (res.length < 3) throw new InsufficientException();
         return param.split("/to|/from");
